5b2513c586d4c3d2b32b8e32d1e023e5761d2641
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-08-28 13:24:41 +0530

    FormValidation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Form'
+
+function App() {
+return (
+    <div  id="d1" className='border border-dark border-1 m-1 p-5'>
+    <h3 style={{textDecoration:"underline",marginBottom:"6%"}}>Form Validation Using Formik <span style={{paddingLeft:"50%"}}>Duration-2weeks</span></h3>
+    <div>
+    <Form></Form>
+    </div>
+    </div>
+  )
+}
+
+export default App
